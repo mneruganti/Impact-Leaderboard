0c6fe80a15321347cac9c8873a2dff79e404c77c
Manasa <m.neruganti@gmail.com>
2025-08-05 17:27:35 -0400

    Fixed problem with starting up the backend on a port

--- server.js
+++ server.js
@@ -6,14 +6,21 @@ const userRoutes = require('./server/routes/users');
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = 5000;
 
 // mongoose.connect('mongodb://localhost:27017/impact-os', {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
 
-app.use('/api/users', userRoutes);
-
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
-});
+app.get('/api/leaderboard', (req, res) => {
+    const topTen = leaderboardData
+      .sort((a, b) => b.rippleScore - a.rippleScore)
+      .slice(0, 10);
+    res.json(topTen);
+  });
+  
+  // Start server
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
